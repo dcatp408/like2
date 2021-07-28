4e84664c16ca7647b87e2dcdb88d4f0419d53ebc
Duy Pham <Duy@Users-MacBook-Pro-2.local>
2021-07-27 20:24:26 -0700

    intitial upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var firstLabel = document.querySelector('.label')
+var firstLikes = 9
+function likeIncrease1() {
+    firstLikes++
+    firstLabel.innerText = firstLikes + " like(s)"
+}
+var secondLabel = document.querySelector('.label2')
+var secondLikes = 12
+function likeIncrease2() {
+    secondLikes++
+    secondLabel.innerText = secondLikes + " like(s)"
+}
+var thirdLabel = document.querySelector('.label3')
+var thirdLikes = 9
+function likeIncrease3() {
+    thirdLikes++
+    thirdLabel.innerText = thirdLikes + " like(s)"
+}
